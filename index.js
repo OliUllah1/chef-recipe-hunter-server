87061a0c3046a9cc9059f1578b75368393a2a690
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-05-02 23:17:07 +0600

    client side data fetch problem solve

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000
 
 const allChefInfo = require('./data/chefInfo.json')
 
+app.use(cors())
 // app.get('/',(req,res)=>{
 //     res.send('hello world')
 // })
