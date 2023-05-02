0a468295d67fda19c41f4c969298e9bea2d195cf
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-05-02 22:32:03 +0600

    display all chef data local server

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const allChefInfo = require('./data/chefInfo.json')
 
+// app.get('/',(req,res)=>{
+//     res.send('hello world')
+// })
 app.get('/',(req,res)=>{
-    res.send('hello world')
+    res.send(allChefInfo)
 })
 
 app.listen(port, () => {
