7562c1dea409ef831b72c41ce02de1d487db9bee
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-05-03 08:48:26 +0600

    dynamic data load of  id

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const port = 5000
 const allChefInfo = require('./data/chefInfo.json')
 
 app.use(cors())
-// app.get('/',(req,res)=>{
-//     res.send('hello world')
-// })
+
 app.get('/',(req,res)=>{
     res.send(allChefInfo)
 })
 
+app.get('/:id',(req,res)=>{
+    const id = req.params.id
+    const chefInfo = allChefInfo.find(chef => chef.id == id)
+    res.send(chefInfo)
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
